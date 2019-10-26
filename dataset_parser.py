e0b478c6f1a26b36b9a90cf0b50e351d02f5dc61
alsuvakhitova <paletteknife25>
2019-10-26 23:29:43 +0300

    data+parser

---
+++ dataset_parser.py
@@ -0,0 +1,32 @@
+import glob
+import os
+from skimage.io import imread
+
+
+def get_data(path="./dog_dataset"):
+    """
+    :param path:
+    :return: imgs - list of images
+             nums - list of corresponding numerical breed-values
+             dict{numerical breed: name of breed}
+    """
+    labels = os.listdir(path)
+    labels.remove(".DS_Store")
+    breeds = {}
+    nums = []
+    imgs = []
+    for label in labels[:5]: # TODO: remove bounding
+        label_path = path + "/" + label
+        files = os.listdir(label_path)
+        name = label[(label.find("-")+1):]
+        num = int(label[1:label.find("-")])
+        breeds[num] = name
+        for file in files:
+            file_path = label_path + "/" + file
+            img = imread(file_path)
+            imgs.append(img)
+            nums.append(num)
+    return imgs, nums, breeds
+
+# imgs, nums, breeds = get_data()
+# print(9)
