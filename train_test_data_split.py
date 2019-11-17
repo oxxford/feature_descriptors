edd0c00b467152b2cf214cdd118574bfdb0e5e79
alsuvakhitova <paletteknife25>
2019-11-17 21:45:35 +0300

    Train test split update

--- train_test_data_split.py
+++ train_test_data_split.py
@@ -2,6 +2,7 @@ import random
 from sklearn.model_selection import train_test_split
 from commons.custom_pickle_loader import save_as_pickled_object
 from commons.dataset_parser import get_data
+
 import json
 
 """
@@ -9,23 +10,40 @@ Here we split half of original dataset into train and test sets
 (we use half because of computation limitations)
 """
 
-imgs, nums, breeds = get_data(path="../dog_dataset")
+def main():
+    imgs, nums, breeds = get_data(path="./dog_dataset")
 
-# shuffling original data
-c = list(zip(imgs, nums))
-random.shuffle(c)
-imgs, nums = zip(*c)
+    new=-1
+    unique=0
+    count=0
+    for i in range(len(nums)):
+        if nums[i]!=new:
+            if unique == 60:
+                break
+            else:
+                new = nums[i]
+                unique += 1
+        count+=1
 
-# splitting
-half_len = int(0.5*len(nums))
-imgs_train, imgs_test, nums_train, nums_test = train_test_split(imgs[:half_len], nums[:half_len], test_size=0.2, random_state=42)
-print("# samples in train set: {}".format(len(imgs_train)))
+    print("{} samples will be used".format(count))
 
-# saving
-save_as_pickled_object(imgs_train, "../main_data/imgs_train.pkl")
-save_as_pickled_object(imgs_test, "../main_data/imgs_test.pkl")
-save_as_pickled_object(nums_train, "../main_data/nums_train.pkl")
-save_as_pickled_object(nums_test, "../main_data/nums_test.pkl")
+    # shuffling original data
+    c = list(zip(imgs[:count], nums[:count]))
+    random.shuffle(c)
+    imgs, nums = zip(*c)
 
-with open("breeds.json") as f:
-    json.dumps(breeds)
+    # splitting
+    imgs_train, imgs_test, nums_train, nums_test = train_test_split(imgs, nums, test_size=0.2, random_state=42)
+    print("# samples in train set: {}".format(len(imgs_train)))
+
+    # saving
+    save_as_pickled_object(imgs_train, "./main_data/imgs_train.pkl")
+    save_as_pickled_object(imgs_test, "./main_data/imgs_test.pkl")
+    save_as_pickled_object(nums_train, "./main_data/nums_train.pkl")
+    save_as_pickled_object(nums_test, "./main_data/nums_test.pkl")
+
+    with open("./main_data/breeds.json", 'w') as f:
+        json.dump(breeds, f)
+
+if __name__ == '__main__':
+    main()
